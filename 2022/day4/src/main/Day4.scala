eac32e6f6a76e3d46a17685fb02ff6061c8d87ac
Russell Yeo <rusyeo@gmail.com>
2022-12-04 18:18:13 +0000

    [2022 Day 4] solve part 2

--- 2022/day4/src/main/Day4.scala
+++ 2022/day4/src/main/Day4.scala
@@ -5,25 +5,28 @@ import zio.Console.*
 object Day4 extends ZIOAppDefault {
 
     def solve(filename: String): IO[Throwable, Unit] =
-        stream(filename)
-            .filter(doesFullyContain)
-            .run(ZSink.count)
-            .map(x => println(s"Part One: $x"))
+        for {
+            // Part 1
+            _ <- stream(filename)
+                .filter(doesFullyContain)
+                .run(ZSink.count)
+                .map(x => println(s"Part 1: $x"))
+            // Part 2
+            _ <- stream(filename)
+                .filter(doesOverlap)
+                .run(ZSink.count)
+                .map(x => println(s"Part 2: $x"))
+        } yield ()
 
     /** Returns true if one range fully contains the other */        
     def doesFullyContain(left: Range, right: Range): Boolean =
         left.contains(right.start) && left.contains(right.end) ||
             right.contains(left.start) && right.contains(left.end)
 
-    /** Visualize a pair of ranges */
-    def visualizePair(left: Range, right: Range): IO[Throwable, Unit] =
-        val max = left.end.max(right.end)
-        visualizeRange(left, max) *> visualizeRange(right, max) *> printLine("")
-
-    /** Print a range visually e.g. `3-5` becomes `..345....` */
-    def visualizeRange(range: Range, max: Int): IO[Throwable, Unit] =
-        val line = (1 to max).map { n => if (range.contains(n)) n.toString else "." }.mkString
-        printLine(line)
+    /** Returns true if one range overlaps with the other */     
+    def doesOverlap(left: Range, right: Range): Boolean =
+        left.contains(right.start) || left.contains(right.end) ||
+            right.contains(left.start) || right.contains(left.end)
 
     /** Convert string e.g. `"2-4"` to `Range(2,4)` */
     def toRange(string: String): Range =
@@ -38,6 +41,7 @@ object Day4 extends ZIOAppDefault {
                 val ranges = line.split(",").map(toRange)
                 (ranges(0), ranges(1))
             })
+
     val run = solve("input.txt")
 
 }
