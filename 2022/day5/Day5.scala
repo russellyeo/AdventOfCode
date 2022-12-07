7c3cc6afd1026a9dbe022466f1c3bb6adb487323
Russell Yeo <rusyeo@gmail.com>
2022-12-07 13:00:32 +0000

    [2022 Day 5] write parser and refactor code

--- 2022/day5/Day5.scala
+++ 2022/day5/Day5.scala
@@ -2,28 +2,86 @@
 //> using lib "org.scalatest::scalatest::3.2.14"
 //> using resourceDir "input"
 
-import scala.collection.mutable.Stack
 import scala.io.Source
 import scala.util.{Try, Using}
-import scala.collection.mutable.ArrayBuffer
+import scala.util.chaining._
 
 object Day5 {
 
     case class Action(count: Int, from: Int, to: Int)
+    case class Input(crates: List[List[Char]], actions: List[Action])
     
-    def solve(filename: String, crates: List[List[Char]]): List[Char] =
+    /** 
+     * Parse a given file and find the answer 
+     * 
+     * - Load the file
+     * - Parse the file into the challenge input
+     * - Perform the actions on the crates
+     * - Return the top crate from each stack
+     * */
+    def solve(filename: String): List[Char] =
         load(filename)
-            .map(parse)
-            .foldLeft(crates)(move)
+            .pipe(parse)
+            .pipe(apply)
             .map(_.head)
 
+    /** Move the crates according to the actions */
+    def apply(input: Input): List[List[Char]] =
+        input.actions.foldLeft(input.crates)(move)
+
+    /** Load a list of strings from the file */
     def load(filename: String): List[String] =
         Using(Source.fromResource(filename))(bs => bs.getLines.toList).get
 
-    def parse(input: String): Action =
-        val numbers = ("\\d+".r).findAllIn(input).map(_.toInt).toList
-        Action(numbers(0), numbers(1), numbers(2))
+    /** 
+     * Parse a list of strings into the challenge input 
+     * 
+     * - Split the list of strings into crates and actions (by finding the first empty line)
+     * - Clean up the input (drop the stack numebrs and the empty line)
+     * - Parse the crates and actions into the challenge input
+     * */
+    def parse(lines: List[String]): Input = 
+        lines
+            .span(_.trim.nonEmpty)
+            .pipe((crates, actions) => (crates.dropRight(1), actions.drop(1)))
+            .pipe((crates, actions) => Input(parseCrates(crates), parseActions(actions)))
 
+    /** 
+     * Parse the crates from a list of strings
+     * 
+     * - Transpose the list of strings (rotate the crates 90 degrees)
+     * - Filter by letters
+     * - Filter out whitespace
+     * - Convert to a list
+     * */
+    def parseCrates(input: List[String]): List[List[Char]] =
+        input
+            .transpose
+            .map(_.filter(_.isLetter))
+            .filterNot(_.isEmpty)
+            .toList
+
+    /** 
+     * Parse the actions from a list of strings
+     * 
+     * - Use regex to find the numbers in each string
+     * - Convert to a list of integers
+     * - Convert to an Action
+     * */
+    def parseActions(input: List[String]): List[Action] =
+        input
+            .map(line => ("\\d+".r).findAllIn(line))
+            .map(_.toList.map(_.toInt))
+            .collect { case List(count, from, to) => Action(count, from, to) }
+
+    /** 
+     * Apply an action to a list of stacks of crates
+     * 
+     * Iterate over the stacks of crates, and:
+     * - If it is the 'from' stack, remove the top n crates
+     * - If it is the 'to' stack, add the top n crates from the 'from' stack (but in reverse order)
+     * - Otherwise, leave the stack unchanged
+     * */
     def move(crates: List[List[Char]], action: Action): List[List[Char]] =
         for 
             index <- crates.indices.toList
@@ -35,44 +93,8 @@ object Day5 {
 
 }
 
-object StartingPosition {
-
-    //     [D]    
-    // [N] [C]    
-    // [Z] [M] [P]
-    //  1   2   3 
-    val example = List(
-        List('N', 'Z'),
-        List('D', 'C', 'M'),
-        List('P')
-    )
-
-    //         [M]     [B]             [N]
-    // [T]     [H]     [V] [Q]         [H]
-    // [Q]     [N]     [H] [W] [T]     [Q]
-    // [V]     [P] [F] [Q] [P] [C]     [R]
-    // [C]     [D] [T] [N] [N] [L] [S] [J]
-    // [D] [V] [W] [R] [M] [G] [R] [N] [D]
-    // [S] [F] [Q] [Q] [F] [F] [F] [Z] [S]
-    // [N] [M] [F] [D] [R] [C] [W] [T] [M]
-    //  1   2   3   4   5   6   7   8   9
-    val challenge = List(
-        List('T', 'Q', 'V', 'C', 'D', 'S', 'N'),
-        List('V', 'F', 'M'),
-        List('M', 'H', 'N', 'P', 'D', 'W', 'Q', 'F'),
-        List('F', 'T', 'R', 'Q', 'D'),
-        List('B', 'V', 'H', 'Q', 'N', 'M', 'F', 'R'),
-        List('Q', 'W', 'P', 'N', 'G', 'F', 'C'),
-        List('T', 'C', 'L', 'R', 'F', 'W'),
-        List('S', 'N', 'Z', 'T'),
-        List('N', 'H', 'Q', 'R', 'J', 'D', 'S', 'M')
-    )
-
-}
-
 @main
 def app =
     import Day5.*
-    import StartingPosition.*
-    val answer = solve("challenge.txt", challenge)
+    val answer = solve("challenge.txt")
     println(s"Answer: ${answer.mkString("")}")
